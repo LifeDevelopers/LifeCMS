f6f5d4b3385aba2f097bd9fd2c56babf590173e6
Gambya <robertolopesramos@gmail.com>
2014-06-28 07:17:53 -0300

    Inicio do módulo Auditoria.

---
+++ app/modulos/Auditoria/entity/Auditoria.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Classe modelo de Auditoria
+ * @author Roberto Ramos <robertolopesramos@gmail.com>
+ * @version 0.1
+ * @access public
+ */
+include_once('../../Utilizador/entity/Usuario.class.php');
+
+class Auditoria {
+	private $id;
+	private $usuario;
+	private $data;
+	private $tipo;
+	private $descricao;
+	
+	function setId($id) {
+		self::$id = $id;
+	}
+}
+?>
